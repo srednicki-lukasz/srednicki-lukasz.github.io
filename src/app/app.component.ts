9211270aa8bae6ee7b739463fb005be7e6dc6c73
srednicki-lukasz <srednickilukasz@gmail.com>
2025-05-08 23:42:55 +0200

    Revert portrait mode lock and hotfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { routerAnimation } from '../animations';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -12,14 +12,6 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   imports: [RouterOutlet, SidebarComponent],
 })
 export class AppComponent {
-  public isPortrait = signal(window.matchMedia('(orientation: portrait)').matches);
-
-  constructor() {
-    window
-      .matchMedia('(orientation: portrait)')
-      .addEventListener('change', ({ matches: portrait }) => this.isPortrait.set(portrait));
-  }
-
   public prepareRoute(outlet: RouterOutlet): boolean {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { PortfolioHttpService } from './store/portfolio-http.service';
 import { PortfolioStore } from './store/portfolio.store';
+import { HomeComponent } from './containers/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     data: { animation: 'home' },
-    loadComponent: () => import('./containers/home/home.component').then(m => m.HomeComponent),
+    component: HomeComponent,
   },
   {
     path: 'projects',
